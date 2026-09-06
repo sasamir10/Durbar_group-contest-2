83013df8550afe2d5dca8b34cdf90349a8eff737
sasamir10 <sabbirahmed.samir18@gmail.com>
2026-09-06 13:42:22 +0600

    Durbar gorup contest 2

---
+++ problem1.js
@@ -0,0 +1,15 @@
+function toCamelCase(sentence) {
+    return sentence
+        .trim()
+        .split(/\s+/)
+        .map((word, index) => {
+            word = word.toLowerCase();
+            if (index === 0) return word;
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+        .join("");
+}
+
+console.log(toCamelCase("hello world example"));
+console.log(toCamelCase("Convert THIS Sentence"));
+console.log(toCamelCase("  extra   spaces   here "));
